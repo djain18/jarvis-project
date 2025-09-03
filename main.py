ab9f70d2b85f2778480e04932703c32299799f53
Daksh Jain <dakshinjain187@gmail.com>
2025-09-04 01:44:18 +0530

    fix: try fixing "jarvis active" not speaking

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
-import pyttsx3# used for text to speech converter
+import pyttsx3  # used for text to speech converter
+
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
@@ -9,20 +10,32 @@ def speak(text="No Text Given"):
     engine.runAndWait()
 
 if __name__ == "__main__":
-    speak('initializing jarvis....')
+    speak('Initializing Jarvis....')
     # Listen for the word "Jarvis"
     while True:
-        with sr.Microphone() as source:
-            print("Listening...")
-            """
-            timeout waits the given amount of seconds until an error appears
-            phrase_time_limit waits for the amount of seconds before converting
-            """
-            audio = recognizer.listen(source)#, timeout=3, phrase_time_limit=5)
-            print("Recognizing...")
-            try:
-                text = recognizer.recognize_google(audio)
-                print("You said: ", text)
-            except sr.UnknownValueError:
-                print("Sorry, I could not understand audio")
-
+        try:
+            with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source, duration=0.5)
+                print("Listening...")
+                """
+                timeout waits the given amount of seconds until an error appears
+                phrase_time_limit waits for the amount of seconds before converting
+                """
+                audio = recognizer.listen(source)  
+                print("Recognizing...")
+            word = recognizer.recognize_google(audio)
+          
+            print(word)
+            if("jarvis" in word.lower()):
+                speak("Ya")
+                print("Jarvis Active")
+                with sr.Microphone() as source:
+                    recognizer.adjust_for_ambient_noise(source, duration=0.5)
+                    command_audio = recognizer.listen(source)  
+                    print("Recognizing...")
+                    command_text = recognizer.recognize_google(command_audio)
+                    print(command_text)
+        except sr.UnknownValueError:
+            print("Sorry, I could not understand audio")
+        except sr.RequestError:
+            print("Could not request results, check your internet connection")
